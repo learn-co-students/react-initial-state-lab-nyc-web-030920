38ba5034578d109c78c5200c9d3ac688c3b28f3e
Asilver-jpg <asilver7493@gmail.com>
2020-05-11 20:18:36 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,17 @@
 // your Bomb code here!
+export default class Bomb extends Component{
+    ifBoom(props){
+        let numReturn= `${props} seconds left before I go boom!`
+        props ? numReturn : "Boom!"
+    }
+    
+    constructor(){
+        super()
+        this.state={
+            secondsLeft : props.initialCount
+        }
+    }
+    render(){
+        this.ifBoom(props.initialCount)
+    }
+}
